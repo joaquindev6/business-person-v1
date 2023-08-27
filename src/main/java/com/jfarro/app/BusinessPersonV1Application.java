ef4864170fa7956a7059a1e656d790d5b81278b0
Joaquin Farro <joaquin.fq.dev@gmail.com>
2023-08-26 19:24:50 -0500

    Upload project in github

---
+++ src/main/java/com/jfarro/app/BusinessPersonV1Application.java
@@ -0,0 +1,13 @@
+package com.jfarro.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessPersonV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessPersonV1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/jfarro/app/BusinessPersonV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jfarro.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessPersonV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
