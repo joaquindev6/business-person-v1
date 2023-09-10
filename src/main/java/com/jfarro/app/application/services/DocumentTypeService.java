011408741fcbfd2e7329bc2dc1fbbfdb5b248676
Joaquin Farro <joaquin.fq.dev@gmail.com>
2023-09-09 23:32:00 -0500

    feature: add rest controller documentTypes

--- src/main/java/com/jfarro/app/application/services/DocumentTypeService.java
+++ src/main/java/com/jfarro/app/application/services/DocumentTypeService.java
@@ -1,6 +1,7 @@
 package com.jfarro.app.application.services;
 
-import com.jfarro.app.domain.ports.out.RetrievalDocumentTypeCaseUse;
+import com.jfarro.app.domain.model.DocumentTypeModel;
+import com.jfarro.app.domain.ports.out.RetrievalCaseUse;
 
-public interface DocumentTypeService extends RetrievalDocumentTypeCaseUse {
+public interface DocumentTypeService extends RetrievalCaseUse<DocumentTypeModel> {
 }
--- src/main/java/com/jfarro/app/domain/ports/out/RetrievalDocumentTypeCaseUse.java
+++
@@ -1,9 +0,0 @@
-package com.jfarro.app.domain.ports.out;
-
-import com.jfarro.app.domain.model.DocumentTypeModel;
-import io.reactivex.Observable;
-
-public interface RetrievalDocumentTypeCaseUse {
-    Observable<DocumentTypeModel> findAll();
-    Observable<DocumentTypeModel> findById(Integer documentTypeId);
-}
